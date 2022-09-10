35746270b5f66f9fde666ab8fec0e54017d14362
Kate <katyamalysheva2000@yandex.ru>
2022-09-10 10:19:16 +0300

    added serializer and generic APIView

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .serializers import WomenSerializer
+from .models import Women
+from rest_framework import generics
 # Create your views here.
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    
